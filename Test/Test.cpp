a5cfd0653df2b13f4beed88c3972141438e1f987
한용희 <gksdydrkfl@gmail.com>
2023-08-07 02:14:58 +0900

    test 커밋

---
+++ Test/Test.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+class MyClass
+{
+public:
+	//MyClass();
+
+	int a;
+};
+
+int main()
+{
+	int a;
+	// 객체가 만들어졌다
+	MyClass myClass1;
+	// 인스턴스화가 됬다.
+	MyClass* MyClass2 = new MyClass();
+
+}
